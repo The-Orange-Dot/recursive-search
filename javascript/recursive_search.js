9fdb6c3be7294c2b2263704e6f069155a0a1919d
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-27 13:59:44 +0900

    Completed

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,9 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  if (arr.length === 0) return false;
+
+  if (arr[0] === target) return true;
+
+  return recursiveSearch(arr.slice(1), target);
 }
 
 if (require.main === module) {
@@ -17,3 +21,9 @@ module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//Here, we would need two places to brake: a false case and a true case. For the false case,
+//if we slice the array eventually, we'll have nothing left, so if arr.length === 0, then return
+//false.
+//For the true case, each time the we use a recursion, the arr[0] changes to a a new value of the array.
+//once that value hits the target, the recursion stops and will return true.
